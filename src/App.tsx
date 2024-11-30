f54a4fd35be3a0328dc86f4f6d25f11f19da89c5
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-30 14:43:15 +0330

    [ADD] routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+
 import { createTheme, MantineProvider } from "@mantine/core";
 
+import HomePage from "./pages/home/home.page.tsx";
+
 import "./App.css";
+import SearchPage from "./pages/search/search.page.tsx";
 
 const theme = createTheme({});
 
 function App() {
   return (
     <MantineProvider theme={theme}>
-      <h1>Hello, friend!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   );
 }
---
+++ src/pages/search/search.page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+import styles from "./search.module.css";
+
+export default function SearchPage(): ReactElement {
+  return (
+    <div className={styles.search}>
+      <h1>Search</h1>
+    </div>
+  );
+}
