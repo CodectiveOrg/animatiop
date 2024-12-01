ad3210954e60a74f24fb5dae7141a249bf80b8ba
BijanProgrammer <bijaneisapour@gmail.com>
2024-12-01 13:02:49 +0330

    [ADD] more links

--- src/components/nav/nav.component.tsx
+++ src/components/nav/nav.component.tsx
@@ -4,7 +4,15 @@ import { useLocation, useNavigate } from "react-router";
 
 import { NavLink } from "@mantine/core";
 
-import { IconHome, IconSearch } from "@tabler/icons-react";
+import {
+  IconArticle,
+  IconCategory2,
+  IconFlame,
+  IconHistory,
+  IconHome,
+  IconLogout,
+  IconSearch,
+} from "@tabler/icons-react";
 
 export default function NavComponent(): ReactElement {
   const location = useLocation();
@@ -20,11 +28,47 @@ export default function NavComponent(): ReactElement {
         onClick={() => navigate("/")}
       />
       <NavLink
+        active={location.pathname === "/special-offers"}
+        label="Specia Offers"
+        variant="light"
+        leftSection={<IconFlame />}
+        onClick={() => navigate("/special-offers")}
+      />
+      <NavLink
+        active={location.pathname === "/categories"}
+        label="Categories"
+        variant="light"
+        leftSection={<IconCategory2 />}
+        onClick={() => navigate("/categories")}
+      />
+      <NavLink
         active={location.pathname === "/search"}
         label="Search"
         variant="light"
         leftSection={<IconSearch />}
         onClick={() => navigate("/search")}
+      />
+      <NavLink
+        active={location.pathname === "/history"}
+        label="History"
+        variant="light"
+        leftSection={<IconHistory />}
+        onClick={() => navigate("/history")}
+      />
+      <NavLink
+        active={location.pathname === "/blog"}
+        label="Blog"
+        variant="light"
+        leftSection={<IconArticle />}
+        onClick={() => navigate("/blog")}
+      />
+      <NavLink
+        active={location.pathname === "/sign-out"}
+        label="Sign Out"
+        color="red"
+        variant="light"
+        leftSection={<IconLogout />}
+        onClick={() => navigate("/sign-out")}
       />
     </>
   );
